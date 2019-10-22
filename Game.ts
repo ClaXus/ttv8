f929e7f76ddc620cc11338dd6d989971fb273547
ClaXus <ldas@live.fr>
2019-10-22 23:10:25 +0200

    tt v0.0.0.5

--- Game.ts
+++ Game.ts
@@ -1,11 +1,33 @@
 import {Player} from "./Player";
 import {Board} from "./Board";
+import {Deck} from "./Deck";
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 export class Game{
   player1: Player;
   player2: Player;
   board : Board;
-  timeByTurn : Number;
+  turn : Number;
+  timeInTurn : number;
+  deckPlayer1 : Deck;
+  deckPlayer2 : Deck;
+  timeByTurn : number;
   timestampBegin : Number;
   timestampEnd : Number;
+
+  constructor(player1 : Player, player2 : Player, board : Board, timeByTurn : number, deckPlayer1 : Deck, deckPlayer2 : Deck){
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Board();
+    this.turn = 0;
+    this.timeInTurn = timeByTurn;
+    this.deckPlayer1 = deckPlayer1;
+    this.deckPlayer2 = deckPlayer2;
+    this.timeByTurn = timeByTurn;
+  }
+
+
 }
--- Player.ts
+++ Player.ts
@@ -1,11 +1,9 @@
 import {Deck} from "./Deck";
 
-
 export class Player{
   pseudo : String;
   victories : Number;
   defeats : Number;
   draws : Number;
   deck : Deck;
-
 }
